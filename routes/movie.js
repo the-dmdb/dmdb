d70c19ad5801eddd056e9eff468fedeecfd0da2f
bradymat <bradymat@gmail.com>
2016-10-28 12:29:01 +1300

    handle bars kinda works

--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('displayMovie', { title: 'Movie' });
 });
 
 module.exports = router;
